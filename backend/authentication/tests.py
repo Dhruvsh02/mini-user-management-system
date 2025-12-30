eb47c00e4db59f76a1e8c61c735b842cbbf88c03
Dhruvsh02 <Dhruvsharma110044@gmail.com>
2025-12-30 17:21:31 +0530

    Adding backend unit tests for authentication and admin access

--- backend/authentication/tests.py
+++ backend/authentication/tests.py
@@ -1,3 +1,59 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status 
+from django.urls import reverse
+from users.models import User 
 
-# Create your tests here.
+class SignupTest(APITestCase):
+    def test_signup_success(self):
+        data = {
+            "email": "test1@gmail.com",
+            "full_name": "Testing User",
+            "password": "Test1111"
+        }
+
+        response = self.client.post("/api/auth/signup/", data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(User.objects.filter(email="test1@gmail.com").exists())
+
+
+class LoginTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email="test2@gmail.com",
+            full_name="Login User",
+            password="Login123"
+        )
+
+    def test_login_success(self):
+        data = {
+            "email": "test2@gmail.com",
+            "password": "Login123"
+        }
+
+        response = self.client.post("/api/auth/login/", data, format="json")
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("access", response.data)
+
+class CurrentUserTest(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(
+            email="me@gmail.com",
+            full_name="Current User",
+            password="Curr123"
+        )
+
+        login = self.client.post(
+            "/api/auth/login/",
+            {"email": "me@gmail.com", "password": "Curr123"},
+            format="json"
+        )
+        self.token = login.data["access"]
+
+    def test_get_current_user(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.token}")
+
+        response = self.client.get("/api/auth/me/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data["email"], "me@gmail.com")
+
--- backend/users/tests.py
+++ backend/users/tests.py
@@ -1,3 +1,49 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from users.models import User
 
-# Create your tests here.
+class AdminPermissionTest(APITestCase):
+    def setUp(self):
+        self.admin_user = User.objects.create_user(
+            email="normal@gmail.com",
+            full_name="Normal User",
+            password="Nor123",
+        )
+
+        login = self.client.post(
+            "/api/auth/login/",
+            {"email": "normal@gmail.com", "password": "Nor123"},
+            format="json"
+        )
+
+        self.token = login.data["access"]
+
+    def test_non_admin_access_admin_api(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.token}")
+
+        response = self.client.get("/api/admin/users/")
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+class AdminAccessTest(APITestCase):
+    def setUp(self):
+        self.admin_user = User.objects.create_user(
+            email="admin@gmail.com",
+            full_name="Admin User",
+            password="Admin123",
+            role="admin",
+            is_staff=True,
+        )
+        login = self.client.post(
+            "/api/auth/login/",
+            {"email": "admin@gmail.com", "password": "Admin123"},
+            format="json"
+        )
+
+        self.token = login.data["access"]
+    
+    def test_admin_can_list_users(self):
+        self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {self.token}")
+
+        response = self.client.get("/api/admin/users/")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("results", response.data)

259dabe7a77885d6c499ac61a919b272b83f18ee
Dhruvsh02 <Dhruvsharma110044@gmail.com>
2025-12-30 14:21:36 +0530

    Add JWT-based login API

--- backend/authentication/serializers.py
+++ backend/authentication/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from users.models import User
 import re 
+from django.contrib.auth import authenticate
 
 
 class SignupSerializer(serializers.ModelSerializer):
@@ -30,4 +31,23 @@ class SignupSerializer(serializers.ModelSerializer):
             full_name=validated_data["full_name"],
             password=validated_data["password"]
         )
+    
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        user = authenticate(
+            email=data["email"],
+            password=data["password"]
+        )
+
+        if not user:
+            raise serializers.ValidationError("Invalid email or password.")
+        
+        if not user.is_active:
+            raise serializers.ValidationError("User account is inactive.")
+        
+        data["user"] = user
+        return data
     
